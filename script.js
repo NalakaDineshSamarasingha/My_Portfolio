d9aaa00ee9b17b4ddaf7b9913ad066d7b922f809
NalakaDinesh <133525019+NalakaDineshSamarasingha@users.noreply.github.com>
2024-01-06 00:24:36 +0530

    Some change in responsive

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function myFunction(){
+function myFunction(y){
 var x = document.getElementById("hide2");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
+
   }
+  y.classList.toggle("change");
 }
